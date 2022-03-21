314c5ef34240de5134281b89e9b20ae21cf6ef3a
ArkoniX21 <chernovsergey21@yandex.ru>
2022-03-21 20:12:51 +0300

    3

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,15 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col'
 import NavBar from './Components/NavBar';
+import MainPage from './Pages/MainPage';
+import {BrowserRouter} from 'react-router-dom';
 
 const App = () => {
   return (
+    <div>
     <NavBar />
+    <MainPage />
+    </div>
   );
 };
 
---
+++ client/src/Pages/MainPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+
+const MainPage = () => {
+    return (
+        <Container>
+            <Row>
+                <Col>
+                    WELCOME TO THE MAINPAGE
+                </Col>
+            </Row>
+        </Container>
+    );
+};
+
+export default MainPage;
